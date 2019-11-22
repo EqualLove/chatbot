6e49d5eaefd7b675efd2c5d3202ce98932b47897
EqualLove <ckck1212@naver.com>
2019-11-22 18:53:18 +0900

    Delete app.spec.js

--- app.spec.js
+++
@@ -1,9 +0,0 @@
-const sayHello = require('./app').sayHello;
-const assert = require('assert');
-
-describe('App test!', function () {
-	it('sayHello should return hello', function (done) {
-		assert.equal(sayHello(),'hello');
-		done();
-	});
-});
